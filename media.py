75c863ef45a69f69b0817f12ea79e064192a23e9
Tolga Boztuna <tboztuna@yahoo.com>
2017-07-02 01:18:30 +0300

    added parantheses after class name for the python's backward compatibility

--- media.py
+++ media.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 
-class Movie:
+class Movie():
     """This class provides a way to store movie related information"""
 
     def __init__(self, title, poster_image_url, trailer_youtube_url):
