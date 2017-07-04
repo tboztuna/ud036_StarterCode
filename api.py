e86d04bff510f49c5832d752522bf2695419e59b
Tolga Boztuna <tboztuna@yahoo.com>
2017-07-05 01:42:37 +0300

    the movie db api integrated

--- api.py
+++ api.py
@@ -1,25 +1,87 @@
+import json
 import urllib
 
 class TheMovieDb():
+    """This class provides a way to connect to the movie db api
+    to get related information about the films
+    """
 
     __discover_url = "https://api.themoviedb.org/3/discover/movie"
+    __movie_details_url = "https://api.themoviedb.org/3/movie/"
 
     def __init__(self, api_key):
         self.__api_key = api_key
 
     def set_api_key(self, api_key):
+        """Sets api key
+            Parameters:
+                api_key
+        """
         self.__api_key = api_key
 
     def get_api_key(self):
+        """Returns api key.
+            Returns:
+            str: The API Key.
+        """
         return self.__api_key
 
     def build_discover_url(self):
-        url = self.__discover_url + "?api_key=" + self.get_api_key()
+        """Builds and returns discover url.
+            Returns:
+            str: The discover url.
+        """
+        url = self.__discover_url + "?api_key=" + self.get_api_key() + "&sort_by=popularity.desc"
+        return url
+
+    def build_movie_url(self, movie_id):
+        """Builds and returns the movie url.
+            Parameters:
+                movie_id
+            Returns:
+            str: The movie url.
+        """
+        url = self.__movie_details_url + str(movie_id) + "/videos?api_key=" + self.get_api_key()
         return url
 
     def get_discover_url(self):
+        """Returns builded discover url.
+            Returns:
+            str: The discover url.
+        """
         return self.build_discover_url()
 
+    def get_movie_url(self, movie_id):
+        """Returns builded movie url.
+            Parameters:
+                movie_id
+            Returns:
+            str: The movie url.
+        """
+        return self.build_movie_url(movie_id)
+
     def discover(self):
+        """Gets randomly generated film data.
+            Returns:
+            str: json data of movies.
+        """
         contents = urllib.urlopen(self.get_discover_url())
         return contents.read()
+
+    def get_movie_details_url(self):
+        """Returns movie_details url.
+            Returns:
+            str: The movie_details url.
+        """
+        return self.__movie_details_url
+
+    def get_trailer_url(self, movie_id):
+        """Returns the trailer url of the movie.
+            Parameters:
+                movie_id
+            Returns:
+            str: The trailer url.
+        """
+        contents = urllib.urlopen(self.get_movie_url(movie_id))
+        data = json.loads(contents.read())
+        return "https://www.youtube.com/watch?v=" + data["results"][0]["key"]
--- entertainment_center.py
+++ entertainment_center.py
@@ -5,11 +5,13 @@ import fresh_tomatoes
 
 the_movie_db = api.TheMovieDb("55ed321f0e57e3983da866087cb460f6")
 contents = json.loads(the_movie_db.discover())
-print contents
-movies = list()
 
+movies = list()
 for item in contents["results"]:
-    movie_instance = media.Movie(item["title"], item["poster_path"], "")
-    movies.append(movie_instance)
+    movie_id = item["id"]
+    movie_trailer_url = the_movie_db.get_trailer_url(movie_id)
+    movie_title = item["title"][:20] if len(item["title"]) < 20  else  item["title"][:20] + "..."
+    movie_object = media.Movie(movie_title, item["poster_path"], movie_trailer_url)
+    movies.append(movie_object)
 
 fresh_tomatoes.open_movies_page(movies)
